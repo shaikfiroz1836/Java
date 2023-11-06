bad1016ae420d826ac799b007f20c842a00f6c21
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2023-11-06 17:59:24 +0530

    Test

---
+++ Test1.java
@@ -0,0 +1,7 @@
+class Test1{
+  public static void main(String[] args){
+	  int[] num = {101,102,103,104};
+     System.out.println("Hello Good Morning, I am Firoz");
+	 System.out.println(num[1]);
+    }
+}
