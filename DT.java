67b2665825b4a41ca808b0c9c8dfdb8dd8c50204
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2023-11-06 18:04:24 +0530

    Test

---
+++ DT.java
@@ -0,0 +1,12 @@
+class DT{
+  public static void main(String[] args){
+	  byte num1= 1;
+	  short num2 = 129;
+	  int num3= 4335;
+	  long num4=7386273738l;
+     System.out.println(num1);
+	 System.out.println(num2);
+	 System.out.println(num3);
+	 System.out.println(num4);
+    }
+}
