fc0f5d0de79847589d66713d5801d2b4641b6106
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2023-11-07 11:05:51 +0530

    Test

---
+++ Arrays/Arrays.java
@@ -0,0 +1,9 @@
+class Arrays{
+	public static void main(String[] firoz){
+		int[] ids={101,102,103,104};
+		System.out.println(ids[0]);
+		System.out.println(ids[1]);
+		System.out.println(ids[2]);
+		System.out.println(ids[3]);
+	}
+}
