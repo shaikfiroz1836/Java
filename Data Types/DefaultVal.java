e15c12f292c9de639bae242d26552ee01c3eb43f
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2023-11-07 11:17:03 +0530

    Default Values

---
+++ Data Types/DefaultVal.java
@@ -0,0 +1,24 @@
+class DefaultVal{
+	byte b;
+	  short s;
+	  int i;
+	  long l;
+	  float f;
+	  double d;
+	  char c;
+	  boolean avail;
+	  
+	  String st;
+  public static void main(String[] args){
+	  DefaultVal t1 = new DefaultVal();
+     System.out.println(t1.b);
+	 System.out.println(t1.s);
+	 System.out.println(t1.i);
+	 System.out.println(t1.l);
+	 System.out.println(t1.f);
+	 System.out.println(t1.d);
+	 System.out.println(t1.c);
+	 System.out.println(t1.avail);
+	 System.out.println(t1.st);
+    }
+}
