bdf93116bf30967220aa5f5035a051afd310b83b
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2023-11-08 13:16:15 +0530

    Practice

---
+++ Practice/Practice.java
@@ -0,0 +1,13 @@
+class Practice{
+	int eid=1;
+	String ename="Firoz";
+	double salary=45000;
+	
+	public static void main(String[] firoz){
+		Practice p = new Practice();
+		
+		System.out.println(p.eid);
+		System.out.println(p.ename);
+		System.out.println(p.salary);
+	}
+}
