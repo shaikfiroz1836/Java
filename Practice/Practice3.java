44e54c10fb6efce914a168fda80265fe8897a1ed
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2023-11-15 11:34:21 +0530

    Practice

---
+++ Practice/Practice3.java
@@ -0,0 +1,34 @@
+class Practice3{
+	int eid=1;
+	String ename="Firoz";
+	double salary=45000;
+	String add = "Narasaraopet,Palnadu dist,AP";
+	
+	public static void main(String[] firoz){
+		Practice p = new Practice();
+		
+		System.out.println("Employee is is : " + p.eid);
+		System.out.println("Employee name is : " + p.ename);
+		System.out.println("Employee Salary is : " + p.salary);
+		System.out.println("Employee Address is : " + p.add);
+		
+	}
+}
+class B{
+	int eid=2;
+	String ename="Mahammad";
+	double salary=65000;
+	String add = "Narasaraopet,Palnadu dist,AP";
+	
+	public static void main(String[] firoz){
+		B b = new B();
+		
+		System.out.println("Employee is is : " + b.eid);
+		System.out.println("Employee name is : " + b.ename);
+		System.out.println("Employee Salary is : " + b.salary);
+		System.out.println("Employee Address is : " + b.add);
+	}
+}
+
+
+
