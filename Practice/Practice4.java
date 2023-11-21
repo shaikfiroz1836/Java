07874f6c5fb93be81a19061a5a382c678ca9db0a
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2023-11-21 09:50:16 +0530

    Practice4

---
+++ Practice/Practice4.java
@@ -0,0 +1,13 @@
+class Practice4{
+	public static void main(String[] firoz){
+		int a = 10;
+		int b = 15;
+		if(a> b){
+			System.out.println("A is grater than B");
+		}else if(a<b){
+			System.out.println("A is less than B");
+		}else{
+			System.out.println("A is equal to B");
+		}
+	}
+}
