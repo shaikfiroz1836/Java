a661bde5358cb9ab6a1e6441c511a32a9004d989
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2023-11-22 09:42:33 +0530

    Practice6

---
+++ Practice/Practice6.java
@@ -0,0 +1,12 @@
+class Practice6{
+	public static void main(String[] firoz){
+		int[] ids = {1,2,3,4,5};
+		String[] enames = {"Ironman","Captain A","Thor","Hulk","Loki"};
+		for(int i=0;i<=4;i++){
+			System.out.println(ids[i]);
+		}
+		for(int j=0;j<=4;j++){
+			System.out.println(enames[j]);
+		}
+	}
+}
