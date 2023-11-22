64cdb3761055b305c6cbdfc8f8970e848bbb3073
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2023-11-22 11:33:56 +0530

    Test2

---
+++ Test files/squareTest.java
@@ -0,0 +1,11 @@
+Class Test{
+	public static void main(String[] firoz){
+		int Length = int(input);
+		int breadth = int(input);
+		if(Length == breadth){
+			System.out.println("It is a square");
+		}else{
+			System.out.println("It is rectangle");
+		}
+	}
+}
