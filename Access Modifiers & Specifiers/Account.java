984fdf15bdbf58de1cdaaa8d96d043337cdcda16
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2023-11-24 21:59:26 +0530

    Modifiers

---
+++ Access Modifiers & Specifiers/Account.java
@@ -0,0 +1,10 @@
+package pack1;
+
+public class Account {
+    public void m1() {
+        System.out.println("M1 Method from pack1");
+    }
+	public static void main(String[] firoz){
+		System.out.println("Main Method from pack1");
+	}
+}
---
+++ Access Modifiers & Specifiers/user.java
@@ -0,0 +1,11 @@
+package pack2;
+
+import pack1.Account;
+
+public class User {
+    public static void main(String[] args) {
+        Account a = new Account();
+        a.m1();
+        System.out.println("Main method from pack2");
+    }
+}
